1a23dc0d7957310af9f7abd51783a8cbc759ceac
MuaviyaImran <muaviya.imran@datumbrain.com>
2025-08-22 17:56:58 +0500

    Removed Default Database URL

--- app/core/config.py
+++ app/core/config.py
@@ -6,9 +6,7 @@ load_dotenv()
 
 
 class Config:
-    DATABASE_URL = os.getenv(
-        "DATABASE_URL", "postgresql://postgres:postgres@localhost/library_db"
-    )
+    DATABASE_URL = os.getenv("DATABASE_URL", "")
     APP_NAME = os.getenv("APP_NAME", "Library Management API")
     APP_VERSION = os.getenv("APP_VERSION", "1.0.0")
     DEBUG = os.getenv("DEBUG", "true") == "true"
